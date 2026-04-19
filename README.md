a1338aa267ab38d703e4b7b4d36cb518fffbda95
Nermeen Nedal <nermeennedal11@gmail.com>
2026-04-20 00:47:18 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Library_Agent_Advisor
+
---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE books(
+isbn TEXT PRIMARY KEY ,
+title TEXT  NOT NULL,
+author TEXT NOT NULL ,
+quantity INT DEFAULT 0,
+price REAL DEFAULT 0 
+); -- books info 
+
+
+
