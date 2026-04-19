6c17fe2078e9fe4c0eca5b873d9f2ef193b3606a
Nermeen Nedal <nermeennedal11@gmail.com>
2026-04-20 01:38:24 +0300

    seed

---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO books(isbn,title,author,quantity,price)
+VALUES
+('9789953890024','the alchemist','paulo coelho',7,14.50),
+('9789770930927', 'nesyan.com (forgetfulness.com)', 'ahlam mosteghanemi', 5, 12.00),
+('9789953270192', 'memory in the flesh', 'ahlam mosteghanemi', 9, 13.75),
+('9786140102665', 'the forty rules of love', 'elif shafak', 6, 15.20),
+('9789774162348', 'in my heart a hebrew woman', 'khawla hamdi', 8, 11.90),
+('9789953685262', 'the bamboo stalk', 'saud alsanousi', 4, 16.00),
+('9789953688850', 'a small death', 'mohammed hassan alwan', 3, 18.25),
+('9789953271588', 'black suits you', 'ahlam mosteghanemi', 10, 12.80),
+('9789953270123', 'the yacoubian building', 'alaa al aswany', 6, 14.10),
+('9789953273216', 'the da vinci code (arabic edition)', 'dan brown', 5, 17.50);
+
+INSERT INTO customers(id,email,name)
+VALUES
+(1,'nermeennedal11@gmail.com','Nermeen nedal'),
+(2,'Shathanedal@yahoo.com','Shatha dawoud'),
+(3,'Sallyhaji@outlook.com','sally haji');
