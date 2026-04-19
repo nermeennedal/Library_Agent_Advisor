831ce5298748fd849506101a3ace5a3b35d40d51
Nermeen Nedal <nermeennedal11@gmail.com>
2026-04-20 01:15:14 +0300

    initial

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+
+
+--Domain Tables 
+
+-- books table
 CREATE TABLE books(
 isbn TEXT PRIMARY KEY ,
 title TEXT  NOT NULL,
@@ -5,6 +10,45 @@ author TEXT NOT NULL ,
 quantity INT DEFAULT 0,
 price REAL DEFAULT 0 
 ); -- books info 
+
+
+
+-- Customers table 
+CREATE TABLE customers(
+id INT PRIMARY KEY AUTOINCREMENT,
+email TEXT NOT NULL,
+name TEXT NOT NULL,
+);
+
+
+--orders 
+
+CREATE TABLE orders (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+customer_id INT NOT NULL ,
+status TEXT DEFAULT 'pending',
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+);
+
+--order items
+CREATE TABLE order_items (
+    order_id INT NOT NULL,
+    isbn TEXT NOT NULL,
+    quantity INT NOT NULL ,
+    PRIMARY KEY (order_id,isbn) -- two columns primary key 
+);
+
+
+--Chat storge 
+CREATE TABLE messegas (
+id INTEGER DEFAULT AUTOINCREMENT,
+session_id INT NOT NULL,
+role TEXT NOT NULL,
+content TEXT NOT NULL,
+created_at DATETIME DEFAULT TIMESTAMP
+);
+
+
 
 
 
